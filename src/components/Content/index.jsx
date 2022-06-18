eb9e287d211344edcc692632076e70562dbe49f6
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-06-18 18:01:21 -0300

    bug: imagem do qrcode demorava para atualizar

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -19,11 +19,24 @@ function Content({ value, size = '250' }) {
     function handleCatchError() {
         setGetError(true)
     }
+    function QRCODEImage() {
+        return (
+            <figure>
+                <img
+                    loading='lazy'
+                    src={url}
+                    alt={`QrCode gerado pelo valor ${value} `}
+                    onError={handleCatchError}
+                />
+                <figcaption>{value}</figcaption>
+            </figure>
+        )
+    }
 
     return (
         <>
             {
-                !getError ? (
+                value && (!getError ? (
                     <div id="content" className='content'
                         style={{
                             backgroundColor: theme === 'light' ? '#f5f8ff' : '#1f2a48',
@@ -31,19 +44,11 @@ function Content({ value, size = '250' }) {
                             border: theme === 'light' ? '1px solid grey' : 'none'
                         }}
                     >
-                        <figure>
-                            <img
-                                loading='lazy'
-                                src={url}
-                                alt={`QrCode gerado pelo valor ${value} `}
-                                onError={handleCatchError}
-                            />
-                            <figcaption>{value}</figcaption>
-                        </figure>
+                        <QRCODEImage/>
                     </div>
                 ) : (
                     <p>Houve um erro ao carregar a imagem :/</p>
-                )
+                ))
             }
         </>
     );
